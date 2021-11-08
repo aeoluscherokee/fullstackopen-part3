13fd5ad82a7fddc94c636c577e25cc0032fdd6c6
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-08 14:43:48 +0700

    Use cors in the application

--- phonebook/index.js
+++ phonebook/index.js
@@ -2,9 +2,11 @@ const { response } = require("express");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 morgan.token("body", (req) => JSON.stringify(req.body));
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan(":method :url :status :response-time :body"));
 
