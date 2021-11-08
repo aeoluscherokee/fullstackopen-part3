3b5e5363a9a0865c0f22ea21792bcccf7dd6fc33
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-08 17:09:37 +0700

    Modify log message when server starts

--- index.js
+++ index.js
@@ -93,5 +93,5 @@ app.delete("/api/persons/:id", (req, res) => {
 
 const PORT = 3001;
 app.listen(process.env.PORT || PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${process.env.PORT || PORT}`);
 });
