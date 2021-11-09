ec9e237fb6044f3a9a36a8572c49432d11d2ebcf
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-09 20:08:39 +0700

    Prettify code

--- mongo.js
+++ mongo.js
@@ -1,44 +1,44 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
   console.log(
     'Please provide the password as an argument: node mongo.js <password>'
-  )
-  process.exit(1)
+  );
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url = `mongodb+srv://aeoluscherokee:${password}@cluster0.fdpe6.mongodb.net/phonebook?retryWrites=true`
+const url = `mongodb+srv://aeoluscherokee:${password}@cluster0.fdpe6.mongodb.net/phonebook?retryWrites=true`;
 
-mongoose.connect(url)
+mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-})
+});
 
-const Person = mongoose.model('Person', personSchema)
+const Person = mongoose.model('Person', personSchema);
 
 if (process.argv[3] && process.argv[4]) {
-  const name = process.argv[3]
-  const number = process.argv[4]
+  const name = process.argv[3];
+  const number = process.argv[4];
 
   const person = new Person({
     name: name,
     number: number,
-  })
+  });
 
   person.save().then((result) => {
-    console.log(`added ${name} number ${number} to phonebook!`)
-    console.log(result)
-    mongoose.connection.close()
-  })
+    console.log(`added ${name} number ${number} to phonebook!`);
+    console.log(result);
+    mongoose.connection.close();
+  });
 } else if (process.argv.length === 3) {
   Person.find({}).then((persons) => {
     persons.forEach((person) => {
-      console.log(person)
-    })
-    mongoose.connection.close()
-  })
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
 }
