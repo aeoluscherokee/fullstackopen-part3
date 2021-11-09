02426706607ec6e681d703830343a764f3047e8e
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-09 20:08:44 +0700

    Prettify code

--- models/person.js
+++ models/person.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose')
-var uniqueValidator = require('mongoose-unique-validator')
-require('dotenv').config()
+const mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+require('dotenv').config();
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
 mongoose
   .connect(url)
   .then(() => {
-    console.log('connected to MongoDB')
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minLength: 3, unique: true, required: true },
   number: { type: String, minLength: 8, required: true },
-})
+});
 
-personSchema.plugin(uniqueValidator)
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   },
-})
+});
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
