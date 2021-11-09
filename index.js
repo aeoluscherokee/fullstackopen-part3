88f19b8308d62ba47c8d6228c1f394aadd965226
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-09 18:48:17 +0700

    Add validator error handler

--- index.js
+++ index.js
@@ -109,8 +109,8 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === "CastError") {
     return res.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "MongoServerError") {
-    return res.status(500).send({ error: "name must be unique" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).send({ error: "name must be unique" });
   }
 
   next(error);
