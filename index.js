854530b9af208f2e5d6c57444f9294bc37422b69
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-09 19:03:18 +0700

    Change error message sent as response

--- index.js
+++ index.js
@@ -105,12 +105,10 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint);
 
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
-
   if (error.name === "CastError") {
     return res.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
-    return res.status(400).send({ error: "name must be unique" });
+    return res.status(400).send({ error: error.message });
   }
 
   next(error);
