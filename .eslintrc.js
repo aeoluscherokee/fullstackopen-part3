d0dbaef063801dd48662de15a2afca31b6702bce
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-09 20:07:24 +0700

    Remove semicolon rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
-    semi: ['error', 'never'],
     eqeqeq: 'error',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
