72d3e73f862d4eb7d3a2b5d0c67a82c6072a5ae4
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-09 16:04:47 +0700

    Use person module to communicate with MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { response } = require("express");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 
 morgan.token("body", (req) => JSON.stringify(req.body));
 
@@ -11,56 +11,37 @@ app.use(express.json());
 app.use(morgan(":method :url :status :response-time :body"));
 app.use(express.static("build"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
 
 app.get("/info", (req, res) => {
   const now = new Date().toString();
-  res.send(
-    `<p>Phonebook has info for ${persons.length} people</p><p>${now}</p>`
+  Person.find({}).then((persons) =>
+    res.send(
+      `<p>Phonebook has info for ${persons.length} people</p><p>${now}</p>`
+    )
   );
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    return res.json(person);
-  } else {
-    return res.status(404).end();
-  }
+  const id = req.params.id;
+  Person.findById(id).then((person) => {
+    if (person) {
+      res.json(person);
+    } else {
+      res.status(404).end();
+    }
+  });
 });
 
 app.post("/api/persons", (req, res) => {
-  const id = Math.floor(Math.random() * 10000);
   const body = req.body;
   if (!body.name) {
     return res.status(400).json({
@@ -70,25 +51,22 @@ app.post("/api/persons", (req, res) => {
     return res.status(400).json({
       error: "number is missing",
     });
-  } else if (persons.find((person) => person.name === body.name)) {
-    return res.status(409).json({
-      error: "name must be unique",
-    });
   }
-  const person = { id: id, name: body.name, number: body.number };
+  const person = new Person({ name: body.name, number: body.number });
   person.id = id;
-  persons = persons.concat(person);
-  res.json(person);
+  person
+    .save()
+    .then((savedPerson) => res.json(savedPerson))
+    .catch((error) => res.status(400).json(error));
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  if (persons.find((person) => person.id === id)) {
-    persons = persons.filter((person) => person.id !== id);
-    res.status(204).end();
-  } else {
-    res.status(404).end();
-  }
+  const id = req.params.id;
+  Person.findByIdAndRemove(id).then((deletedPerson) => {
+    if (deletedPerson) {
+      res.status(204).end();
+    } else res.status(404).end();
+  });
 });
 
 const PORT = 3001;
